fbacfefadce60d5db92adef27e1a9a38b328813c
Dave Williams <cdave@uw.edu>
2010-11-08 13:47:35 -0800

    Added a nice little plume to the code.

--- mothsmell.py
+++ mothsmell.py
@@ -10,6 +10,7 @@ at any given location on any given timestep.
 
 import numpy as np
 from numpy.random import uniform as ra
+from scipy import interpolate as spl
 from numpy import pi, sin, cos
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
@@ -18,6 +19,43 @@ from animation import FuncAnimation
 BOX_SIZE = (1.0, 1.0, 1.0)
 BODY_LEN = 0.01
 ANT_ANG = np.radians(30)
+
+class plume(object):
+    def __init__(self, source=(ra(0,BOX_SIZE[1]), ra(0,BOX_SIZE[2]))):
+        """A time-evolving odorant for our little guy to flap around in"""
+        self.res = 100.0      #Split into 100 straight segments
+        self.X = self.steps(0, BOX_SIZE[0], self.res) #X locs of plume
+        self.Y = np.zeros_like(self.X) #Y of odor slices
+        self.Z = np.zeros_like(self.X) #Z of same
+        self.source = source #Y/Z of the source
+        self.cross = np.zeros((2, len(self.X)))
+        [self.update() for i in range(self.res)]
+    
+    def cross_breeze(self, wind_samples=5, fade = 0.99):
+        """Puff! How do things get blown around?"""
+        samps = self.steps(0, BOX_SIZE[0], wind_samples-1)
+        for i in [0,1]:
+            new_broom = spl.splrep(samps, ra(-0.1,0.1,wind_samples))
+            clean_sweep = spl.splev(self.X, new_broom)
+            self.cross[i,:] = clean_sweep + fade * self.cross[i,:]
+    
+    def blow_downstream(self, sheer_strength=0.01):
+        """Huff! Move it and sheer it!"""
+        self.Y[1:] = sheer_strength * self.cross[0,1:] + self.Y[:-1]
+        self.Y[0] = sheer_strength * self.cross[0,0] + self.source[0]
+        self.Z[1:] = sheer_strength * self.cross[1,1:] + self.Z[:-1]
+        self.Z[0] = sheer_strength * self.cross[1,0] + self.source[1]
+        
+    def update(self):
+        """Let that wind blow"""
+        self.cross_breeze()
+        self.blow_downstream()
+
+    @staticmethod
+    def steps(lower, upper, steps):
+            """A one, and a two; get from here to there in this many"""
+            l,u,s = lower, upper, steps
+            return np.arange(l,u*(1+0.5*(u-l)/s), float(u-l)/s)
 
 def smell(loc):
     """ What's the smell here?"""
@@ -107,16 +145,28 @@ if __name__ == '__main__':
     ax = p3.Axes3D(fig)
     lines = []
     # Set up some moths
-    moths = [moth((ra(0,1), ra(0,1), ra(0,1))) for i in range(5)]
-    for moth in moths:
-        x, y, z = zip(*moth.location_history)
-        lines.append(ax.plot(x,y,z)[0])
-    def update_moth(num, moths, lines):
-        for m,l in zip(moths, lines):
-            m.fly()
-            x, y, z = zip(*m.location_history)
-            l.set_data(np.array([x,y]))
-            l.set_3d_properties(z)
+    #moths = [moth((ra(0,1), ra(0,1), ra(0,1))) for i in range(5)]
+    #for moth in moths:
+    #    x, y, z = zip(*moth.location_history)
+    #    lines.append(ax.plot(x,y,z)[0])
+    #def update_moth(num, moths, lines):
+    #    for m,l in zip(moths, lines):
+    #        m.fly()
+    #        x, y, z = zip(*m.location_history)
+    #        l.set_data(np.array([x,y]))
+    #        l.set_3d_properties(z)
+    #    return lines
+    # Set up a plume
+    plumes = [plume((0.5,0.5))]
+    for plume in plumes:
+        x, y, z = plume.X, plume.Y, plume.Z
+        lines.append(ax.plot(x, y, z)[0])
+    def update_plume(num, plumes, lines):
+        for plume, line in zip(plumes, lines):
+            plume.update()
+            x, y, z = plume.X, plume.Y, plume.Z
+            line.set_data(np.array([x,y]))
+            line.set_3d_properties(z)
         return lines
     # Make the figure nice
     ax.set_xlim3d([0.0, BOX_SIZE[0]])
@@ -126,6 +176,8 @@ if __name__ == '__main__':
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title("Bouncin' around")
-    moth_ani = FuncAnimation(fig, func=update_moth, frames=100, 
-                             fargs=(moths,lines), interval=10)
+    #moth_ani = FuncAnimation(fig, func=update_moth, frames=100, 
+    #                         fargs=(moths,lines), interval=10)
+    moth_ani = FuncAnimation(fig, func=update_plume, frames=100, 
+                             fargs=(plumes,lines), interval=10)
     plt.show()
